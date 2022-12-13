71e5edcf0ad311ca4d413dfb228a7ceb6c1b49ce
melisand <melisandoval@live.com.ar>
2022-12-13 17:15:11 +0100

    add toggle to like icon

--- src/components/Actions.vue
+++ src/components/Actions.vue
@@ -3,13 +3,23 @@
     <!-- Font Awesome icons -->
     <i class="far fa-comment"></i>
     <i class="fas fa-retweet"></i>
-    <i class="far fa-heart"></i>
+    <i v-if="!likedTweet" class="far fa-heart heart" @click="like"></i>
+    <span v-else @click="like">❤️</span>
     <i class="fas fa-share"></i>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    likedTweet: false,
+  }),
+  methods: {
+    like() {
+      this.likedTweet = !this.likedTweet;
+    },
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped></style>
